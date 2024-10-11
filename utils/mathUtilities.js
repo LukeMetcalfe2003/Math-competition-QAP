60889a52b0ceafd16e70b0fee6fed236feffa195
Luke Metcalfe <luke.metcalfe@keyin.com>
2024-10-11 15:31:08 -0230

    Added getQuestion and isCorrectAnswer functions.

--- utils/mathUtilities.js
+++ utils/mathUtilities.js
@@ -4,6 +4,24 @@
  * @returns {} The randomly generated math question
  */
 function getQuestion() {
+    const num1 = Math.floor(Math.random() * 10) + 1;
+    const num2 = Math.floor(Math.random() * 10) + 1;
+
+    // generate random operator 
+    const operator = ["+", "-", "*", "/"][Math.floor(Math.random() * 4)];
+
+    return{num1, num2, operator};
+
+}
+
+/**
+ * Parses the provided question and gets whether or not the provided answer is correct
+ * 
+ * @param {*} question The question being answered
+ * @param {*} answer The potential answer
+ * @returns {boolean} True if the answer was correct, false otherwise.
+ */
+function isCorrectAnswer(question, answer) {
     const { num1, num2, operator } = question;
     let correctAnswer;
 
@@ -20,18 +38,6 @@ function getQuestion() {
     }
     
     return answer === correctAnswer;
-
-}
-
-/**
- * Parses the provided question and gets whether or not the provided answer is correct
- * 
- * @param {*} question The question being answered
- * @param {*} answer The potential answer
- * @returns {boolean} True if the answer was correct, false otherwise.
- */
-function isCorrectAnswer(question, answer) {
-    return false;
 }
 
 module.exports = {
